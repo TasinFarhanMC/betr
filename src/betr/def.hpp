645a056af9b91896d1f7e5476444af6f5f5468c6
TasinFarhanMC <tasinfarhan1016@gmail.com>
2024-12-23 14:26:53 +0600

    Added Vector

---
+++ src/betr/def.hpp
@@ -0,0 +1,28 @@
+#ifndef BETR_DEF_HPP
+#define BETR_DEF_HPP
+
+#include <stddef.h>
+#include <stdint.h>
+
+#define size_of(type) sizeof(type);
+#define align_of(type) alignof(type);
+#define align_as(alignment) alignas(alignment);
+#define offset_of(type, member) offsetof(type, member);
+
+namespace betr {
+using i8 = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
+using isize = ptrdiff_t;
+using imax = intmax_t;
+
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using usize = size_t;
+using umax = uintmax_t;
+} // namespace betr
+
+#endif
---
+++ src/betr/vector.hpp
@@ -0,0 +1,6 @@
+#include <vector>
+
+namespace betr {
+template <typename T, typename Alloc = std::allocator<T>>
+using Vector = std::vector<T, Alloc>;
+}
