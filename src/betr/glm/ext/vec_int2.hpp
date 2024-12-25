adc79420245d1c81b6493d6a3c901fc6b6d52e69
TasinFarhanMC <tasinfarhan1016@gmail.com>
2024-12-25 09:45:29 +0600

    Started GLM

---
+++ src/betr/glm/ext/vec_int2.hpp
@@ -0,0 +1,10 @@
+#ifndef BETR_GLM_EXT_VEC_INT2_HPP
+#define BETR_GLM_EXT_VEC_INT2_HPP
+
+#include <glm/ext/vector_int2.hpp>
+
+namespace betr {
+using IVec2 = glm::ivec2;
+} // namespace betr
+
+#endif
---
+++ src/betr/glm/vec2.hpp
@@ -0,0 +1,24 @@
+#ifndef BETR_GLM_VEC2_HPP
+#define BETR_GLM_VEC2_HPP
+
+#include "ext/vec_int2.hpp"
+#include <glm/vec2.hpp>
+
+namespace betr {
+using BVec2 = glm::bvec2;
+using UVec2 = glm::ivec2;
+using Vec2 = glm::vec2;
+using DVec2 = glm::dvec2;
+
+using I8Vec2 = glm::i8vec2;
+using I16Vec2 = glm::i8vec2;
+using I32Vec2 = glm::i8vec2;
+using I64Vec2 = glm::i8vec2;
+
+using U8Vec2 = glm::u8vec2;
+using U16Vec2 = glm::u8vec2;
+using U32Vec2 = glm::u8vec2;
+using U64Vec2 = glm::u8vec2;
+} // namespace betr
+
+#endif
