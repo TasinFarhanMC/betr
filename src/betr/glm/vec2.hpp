1f3ac3ab681456299f332d1a6d734f68fe0e269c
TasinFarhanMC <tasinfarhan1016@gmail.com>
2025-05-31 11:32:32 +0600

    Re-Added Glm

--- src/betr/glm/vec2.hpp
+++ src/betr/glm/vec2.hpp
@@ -1,10 +1,24 @@
 #ifndef BETR_GLM_VEC2_HPP
 #define BETR_GLM_VEC2_HPP
 
-#include "ext/vec_bool2.hpp"
-#include "ext/vec_double2.hpp"
-#include "ext/vec_float2.hpp"
-#include "ext/vec_int2_sized.hpp"
-#include "ext/vec_uint2_sized.hpp"
+#include "ext/vec_int2.hpp"
+#include <glm/vec2.hpp>
+
+namespace betr {
+using BVec2 = glm::bvec2;
+using UVec2 = glm::ivec2;
+using Vec2 = glm::vec2;
+using DVec2 = glm::dvec2;
+
+using I8Vec2 = glm::i8vec2;
+using I16Vec2 = glm::i8vec2;
+using I32Vec2 = glm::i8vec2;
+using I64Vec2 = glm::i8vec2;
+
+using U8Vec2 = glm::u8vec2;
+using U16Vec2 = glm::u8vec2;
+using U32Vec2 = glm::u8vec2;
+using U64Vec2 = glm::u8vec2;
+} // namespace betr
 
 #endif
---
+++ src/betr/glm/vec4.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <glm/vec4.hpp>
+
+namespace betr {
+using Vec4 = glm::vec4;
+} // namespace betr
